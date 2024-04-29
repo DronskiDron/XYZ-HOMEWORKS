0046f4745de53ac68101594d39c27d33ee716728
DronskiDron <andronaf93@gmail.com>
2024-04-29 17:24:09 +0300

    Домашняя работа №19.

---
+++ Assets/Scripts/UI/Localization/LocalizeText.cs
@@ -0,0 +1,32 @@
+﻿using Creatures.Model.Definitions.Localisation;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Localization
+{
+    [RequireComponent(typeof(Text))]
+    public class LocalizeText : AbstractLocalizeComponent
+    {
+        [SerializeField] private string[] _keys;
+        [SerializeField] private bool _capitalize;
+
+        private Text _text;
+
+
+        protected override void Awake()
+        {
+            _text = GetComponent<Text>();
+            base.Awake();
+        }
+
+
+        protected override void Localize()
+        {
+            foreach (var key in _keys)
+            {
+                var localized = LocalizationManager.I.Localize(key);
+                _text.text = _capitalize ? localized.ToUpper() : localized;
+            }
+        }
+    }
+}
